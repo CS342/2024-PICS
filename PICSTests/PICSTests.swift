74b7cd64eb435c30f9cdea175557f0f409ea3dec
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2024-01-12 00:20:27 -0800

    Initial Setup (#1)
    # Initial Setup
    
    ## :gear: Release Notes 
    - Initial Setup of the CS342 2024 Stronger application
    
    ## :pencil: Code of Conduct & Contributing Guidelines 
    
    By submitting creating this pull request, you agree to follow our [Code
    of
    Conduct](https://github.com/CS342/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/CS342/.github/blob/main/CONTRIBUTING.md):
    - [x] I agree to follow the [Code of
    Conduct](https://github.com/CS342/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/CS342/.github/blob/main/CONTRIBUTING.md).

---
+++ PICSTests/PICSTests.swift
@@ -0,0 +1,17 @@
+//
+// This source file is part of the PICS based on the Stanford Spezi Template Application project
+//
+// SPDX-FileCopyrightText: 2023 Stanford University
+//
+// SPDX-License-Identifier: MIT
+//
+
+@testable import PICS
+import XCTest
+
+
+class PICSTests: XCTestCase {
+    func testExample() throws {
+        XCTAssertTrue(true)
+    }
+}
