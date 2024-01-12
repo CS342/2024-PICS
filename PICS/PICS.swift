e11f2a3488f9512944e9a10d5b0bc8d4968b72aa
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2024-01-12 00:20:27 -0800

    Initial Setup (#1)
    # Initial Setup
    
    ## :gear: Release Notes
    - Initial Setup of the CS342 2024 Stronger application
    
    ## :pencil: Code of Conduct & Contributing Guidelines
    
    By submitting creating this pull request, you agree to follow our [Code
    of
    Conduct](https://github.com/CS342/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/CS342/.github/blob/main/CONTRIBUTING.md):
    - [x] I agree to follow the [Code of
    Conduct](https://github.com/CS342/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/CS342/.github/blob/main/CONTRIBUTING.md).

---
+++ PICS/PICS.swift
@@ -0,0 +1,36 @@
+//
+// This source file is part of the PICS based on the Stanford Spezi Template Application project
+//
+// SPDX-FileCopyrightText: 2023 Stanford University
+//
+// SPDX-License-Identifier: MIT
+//
+
+import Spezi
+import SpeziFirebaseAccount
+import SwiftUI
+
+
+@main
+struct PICS: App {
+    @UIApplicationDelegateAdaptor(PICSDelegate.self) var appDelegate
+    @AppStorage(StorageKeys.onboardingFlowComplete) var completedOnboardingFlow = false
+
+    
+    var body: some Scene {
+        WindowGroup {
+            ZStack {
+                if completedOnboardingFlow {
+                    HomeView()
+                } else {
+                    EmptyView()
+                }
+            }
+                .sheet(isPresented: !$completedOnboardingFlow) {
+                    OnboardingFlow()
+                }
+                .testingSetup()
+                .spezi(appDelegate)
+        }
+    }
+}
