74108e92473cc58e57bacef0bef2389712756475
karansoin <64022954+karansoin@users.noreply.github.com>
2024-02-05 20:40:58 -0800

    added multiple quesionnaires with working completion logo

--- PICS/Schedule/EventContextView.swift
+++ PICS/Schedule/EventContextView.swift
@@ -13,46 +13,72 @@ import SwiftUI
 struct EventContextView: View {
     let eventContext: EventContext
     
-    
     var body: some View {
         HStack {
-            VStack(alignment: .leading) {
-                HStack {
-                    if eventContext.event.complete {
-                        Image(systemName: "checkmark.circle.fill")
-                            .foregroundColor(.accentColor)
-                            .font(.system(size: 30))
-                            .accessibilityHidden(true)
-                    }
-                    VStack(alignment: .leading, spacing: 8) {
-                        Text(verbatim: eventContext.task.title)
-                            .font(.headline)
-                            .accessibilityLabel(
-                                eventContext.event.complete
-                                    ? "COMPLETED_TASK_LABEL \(eventContext.task.title)"
-                                    : "TASK_LABEL \(eventContext.task.title)"
-                            )
-                        Text(verbatim: format(eventDate: eventContext.event.scheduledAt))
-                            .font(.subheadline)
-                    }
-                }
-                Divider()
-                Text(eventContext.task.description)
-                    .font(.callout)
-                if !eventContext.event.complete {
-                    Text(eventContext.task.context.actionType)
-                        .frame(maxWidth: .infinity, minHeight: 50)
-                        .foregroundColor(.white)
-                        .background(Color.accentColor)
-                        .clipShape(RoundedRectangle(cornerRadius: 8))
-                        .padding(.top, 8)
-                }
-            }
+            eventDetailsStack
         }
-            .disabled(eventContext.event.complete)
-            .contentShape(Rectangle())
+        .disabled(eventContext.event.complete)
+        .contentShape(Rectangle())
     }
     
+    private var eventDetailsStack: some View {
+        VStack(alignment: .leading) {
+            HStack(alignment: .top) { // Align items to top
+                statusIcon
+                    .padding(.top, 5) // Adjust padding to align with the first line of text if needed
+                titleAndDateStack
+            }
+            Divider()
+            descriptionText
+            if !eventContext.event.complete {
+                actionTypeText
+            }
+        }
+    }
+    
+    private var statusIcon: some View {
+        Group {
+            if eventContext.event.complete {
+                Image(systemName: "checkmark.circle.fill")
+                    .foregroundColor(.green)
+                    .font(.system(size: 30))
+                    .accessibilityHidden(true)
+            } else {
+                Image(systemName: "exclamationmark.circle.fill")
+                    .foregroundColor(.yellow)
+                    .font(.system(size: 30))
+                    .accessibilityHidden(true)
+            }
+        }
+    }
+    
+    private var titleAndDateStack: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            Text(verbatim: eventContext.task.title)
+                .font(.headline)
+                .accessibilityLabel(
+                    eventContext.event.complete
+                        ? "COMPLETED_TASK_LABEL \(eventContext.task.title)"
+                        : "TASK_LABEL \(eventContext.task.title)"
+                )
+            Text(verbatim: format(eventDate: eventContext.event.scheduledAt))
+                .font(.subheadline)
+        }
+    }
+    
+    private var descriptionText: some View {
+        Text(eventContext.task.description)
+            .font(.callout)
+    }
+    
+    private var actionTypeText: some View {
+        Text(eventContext.task.context.actionType)
+            .frame(maxWidth: .infinity, minHeight: 50)
+            .foregroundColor(.white)
+            .background(Color.accentColor)
+            .clipShape(RoundedRectangle(cornerRadius: 8))
+            .padding(.top, 8)
+    }
     
     private func format(eventDate: Date) -> String {
         let dateFormatter = DateFormatter()
@@ -62,20 +88,96 @@ struct EventContextView: View {
     }
 }
 
-
 #if DEBUG
-#Preview(traits: .sizeThatFitsLayout) {
-    let task = PICSScheduler.PHQ4Task
-    
-    return EventContextView(
-        eventContext: EventContext(
-            // We use a force unwrap in the preview as we can not recover from an error here
-            // and the code will never end up in a production environment.
-            // swiftlint:disable:next force_unwrapping
-            event: task.events(from: .now.addingTimeInterval(-60 * 60 * 24)).first!,
-            task: task
+struct EventContextView_Previews: PreviewProvider {
+    static var previews: some View {
+        let task = PICSScheduler.PHQ4Task
+        
+        EventContextView(
+            eventContext: EventContext(
+                // We use a force unwrap in the preview as we can not recover from an error here
+                // and the code will never end up in a production environment.
+                // swiftlint:disable:next force_unwrapping
+                event: task.events(from: .now.addingTimeInterval(-60 * 60 * 24)).first!,
+                task: task
+            )
         )
-    )
         .padding()
+    }
 }
 #endif
+
+//struct EventContextView: View {
+//    let eventContext: EventContext
+//    
+//    
+//    var body: some View {
+//        HStack {
+//            VStack(alignment: .leading) {
+//                HStack {
+//                    if eventContext.event.complete {
+//                        Image(systemName: "checkmark.circle.fill")
+//                            .foregroundColor(.green)
+//                            .font(.system(size: 30))
+//                            .accessibilityHidden(true)
+//                    } else {
+//                        Image(systemName:"exclamationmark.circle.fill")
+//                            .foregroundColor(.yellow)
+//                            .font(.system(size: 30))
+//                            .accessibilityHidden(true)
+//                    }
+//                    VStack(alignment: .leading, spacing: 8) {
+//                        Text(verbatim: eventContext.task.title)
+//                            .font(.headline)
+//                            .accessibilityLabel(
+//                                eventContext.event.complete
+//                                    ? "COMPLETED_TASK_LABEL \(eventContext.task.title)"
+//                                    : "TASK_LABEL \(eventContext.task.title)"
+//                            )
+//                        Text(verbatim: format(eventDate: eventContext.event.scheduledAt))
+//                            .font(.subheadline)
+//                    }
+//                }
+//                Divider()
+//                Text(eventContext.task.description)
+//                    .font(.callout)
+//                if !eventContext.event.complete {
+//                    Text(eventContext.task.context.actionType)
+//                        .frame(maxWidth: .infinity, minHeight: 50)
+//                        .foregroundColor(.white)
+//                        .background(Color.accentColor)
+//                        .clipShape(RoundedRectangle(cornerRadius: 8))
+//                        .padding(.top, 8)
+//                }
+//            }
+//        }
+//            .disabled(eventContext.event.complete)
+//            .contentShape(Rectangle())
+//    }
+//    
+//    
+//    private func format(eventDate: Date) -> String {
+//        let dateFormatter = DateFormatter()
+//        dateFormatter.dateStyle = .none
+//        dateFormatter.timeStyle = .short
+//        return dateFormatter.string(from: eventDate)
+//    }
+//}
+//
+//
+//#if DEBUG
+//#Preview(traits: .sizeThatFitsLayout) {
+//    let task = PICSScheduler.PHQ4Task
+//    
+//    return EventContextView(
+//        eventContext: EventContext(
+//            // We use a force unwrap in the preview as we can not recover from an error here
+//            // and the code will never end up in a production environment.
+//            // swiftlint:disable:next force_unwrapping
+//            event: task.events(from: .now.addingTimeInterval(-60 * 60 * 24)).first!,
+//            task: task
+//        )
+//    )
+//        .padding()
+//}
+//#endif
--- PICS/Schedule/PICSScheduler.swift
+++ PICS/Schedule/PICSScheduler.swift
@@ -16,32 +16,6 @@ typealias PICSScheduler = Scheduler<PICSTaskContext>
 
 
 extension PICSScheduler {
-//    static var socialSupportTask: SpeziScheduler.Task<PICSTaskContext> {
-//        let dateComponents: DateComponents
-//        if FeatureFlags.testSchedule {
-//            // Adds a task at the current time for UI testing if the `--testSchedule` feature flag is set
-//            dateComponents = DateComponents(
-//                hour: Calendar.current.component(.hour, from: .now),
-//                minute: Calendar.current.component(.minute, from: .now)
-//            )
-//        } else {
-//            // For the normal app usage, we schedule the task for every day at 8:00 AM
-//            dateComponents = DateComponents(hour: 8, minute: 0)
-//        }
-//
-//        return Task(
-//            title: String(localized: "PHQ-4_TITLE"),
-//            description: String(localized: "PHQ-4_DESCRIPTION"),
-//            schedule: Schedule(
-//                start: Calendar.current.startOfDay(for: Date()),
-//                repetition: .matching(dateComponents),
-//                end: .numberOfEvents(365)
-//            ),
-//            notifications: true,
-//            context: PICSTaskContext.questionnaire(Bundle.main.questionnaire(withName: "PHQ-4"))
-//        )
-//    }
-    
     static var PHQ4Task: SpeziScheduler.Task<PICSTaskContext> {
             let dateComponents: DateComponents
             if FeatureFlags.testSchedule {
@@ -51,8 +25,8 @@ extension PICSScheduler {
                     minute: Calendar.current.component(.minute, from: .now).addingReportingOverflow(1).partialValue
                 )
             } else {
-                // Schedule the task for every day at 6:00 PM for normal app usage
-                dateComponents = DateComponents(hour: 18, minute: 0)
+                // Schedule the task for every day at 8:00 AM for normal app usage
+                dateComponents = DateComponents(hour: 8, minute: 0)
             }
 
             return Task(
@@ -67,9 +41,35 @@ extension PICSScheduler {
                 context: PICSTaskContext.questionnaire(Bundle.main.questionnaire(withName: "PHQ-4"))
             )
         }
+    
+    static var EQ5D5LTask: SpeziScheduler.Task<PICSTaskContext> {
+        let dateComponents: DateComponents
+        if FeatureFlags.testSchedule {
+            // Adds a task at the current time for UI testing if the `--testSchedule` feature flag is set
+            dateComponents = DateComponents(
+                hour: Calendar.current.component(.hour, from: .now),
+                minute: Calendar.current.component(.minute, from: .now)
+            )
+        } else {
+            // For the normal app usage, we schedule the task for every day at 9:00 AM
+            dateComponents = DateComponents(hour: 9, minute: 0)
+        }
+
+        return Task(
+            title: String(localized: "EQ5D5L_TITLE"),
+            description: String(localized: "EQ5D5L_DESCRIPTION"),
+            schedule: Schedule(
+                start: Calendar.current.startOfDay(for: Date()),
+                repetition: .matching(dateComponents),
+                end: .numberOfEvents(365)
+            ),
+            notifications: true,
+            context: PICSTaskContext.questionnaire(Bundle.main.questionnaire(withName: "PHQ-4"))
+        )
+    }
 
     /// Creates a default instance of the ``PICSScheduler`` by scheduling the tasks listed below.
     convenience init() {
-        self.init(tasks: [Self.PHQ4Task])
+        self.init(tasks: [Self.PHQ4Task, Self.EQ5D5LTask])
     }
 }
