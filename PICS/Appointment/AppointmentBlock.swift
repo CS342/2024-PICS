d6014c478f01c274c4142987449cd14656301f62
abhat01 <akanshya@slac.stanford.edu>
2024-02-06 16:35:53 -0800

    Updated licensing information

--- PICS/Appointment/AppointmentBlock.swift
+++ PICS/Appointment/AppointmentBlock.swift
@@ -1,4 +1,12 @@
 //
+// This source file is part of the PICS based on the Stanford Spezi Template Application project
+//
+// SPDX-FileCopyrightText: 2023 Stanford University
+//
+// SPDX-License-Identifier: MIT
+//
+
+//
 //  AppointmentBlock.swift
 //  PICS
 //
