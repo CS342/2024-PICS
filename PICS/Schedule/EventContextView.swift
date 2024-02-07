7bae7a41fd3442544f4bac519c56b43a5f98c8e8
karansoin <64022954+karansoin@users.noreply.github.com>
2024-02-06 19:33:47 -0800

    more swift lint error minor changes

--- PICS/Schedule/EventContextView.swift
+++ PICS/Schedule/EventContextView.swift
@@ -174,19 +174,3 @@ struct EventContextView_Previews: PreviewProvider {
 // }
 //
 //
-// #if DEBUG
-// #Preview(traits: .sizeThatFitsLayout) {
-//    let task = PICSScheduler.PHQ4Task
-//    
-//    return EventContextView(
-//        eventContext: EventContext(
-//            // We use a force unwrap in the preview as we can not recover from an error here
-//            // and the code will never end up in a production environment.
-//            // swiftlint:disable:next force_unwrapping
-//            event: task.events(from: .now.addingTimeInterval(-60 * 60 * 24)).first!,
-//            task: task
-//        )
-//    )
-//        .padding()
-// }
-// #endif
