1e797bf9ea6b10b186e8e180b32167958cfd35ce
karansoin <64022954+karansoin@users.noreply.github.com>
2024-02-08 13:38:06 +0530

    Questionnaire Integration and Scheduling: Karan Soin (#28)
    # Questionnaire Integration and Scheduling: Karan Soin
    
    ## :recycle: Current situation & Problem
    Currently only a very basic implementation of the questionnaire flow,
    and correct UI and content is not present
    
    
    ## :gear: Release Notes 
    - Contains the 3 main required questionnaires for PICS patients
    - Contains UI, text, checkmarks that update when a questionnaire is
    completed
    - Has been scheduled appropriately at 1-hour gaps to ensure it is not
    too overwhelming for PICS patients all at once
    
    
    ## :books: Documentation
    *Please ensure that you properly document any additions in conformance
    to [Spezi Documentation
    Guide](https://github.com/StanfordSpezi/.github/blob/main/DOCUMENTATIONGUIDE.md).*
    FHIR Quesionnaires used, and given sample Spezi app with above
    documentation
    
    
    ## :white_check_mark: Testing
    - Additional testing not required for now, complies with all Swift Lint
    and firebase requirements
    
    
    ## :pencil: Code of Conduct & Contributing Guidelines 
    
    By submitting creating this pull request, you agree to follow our [Code
    of
    Conduct](https://github.com/CS342/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/CS342/.github/blob/main/CONTRIBUTING.md):
    - [x] I agree to follow the [Code of
    Conduct](https://github.com/CS342/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/CS342/.github/blob/main/CONTRIBUTING.md).

--- PICS/Schedule/EventContext.swift
+++ PICS/Schedule/EventContext.swift
@@ -8,18 +8,39 @@
 
 import SpeziScheduler
 
+// struct EventContext: Comparable, Identifiable {
+//    let event: Event
+//    let task: Task<PICSTaskContext>
+//    
+//    
+//    var id: Event.ID {
+//        event.id
+//    }
+//    
+//    
+//    static func < (lhs: EventContext, rhs: EventContext) -> Bool {
+//        lhs.event.scheduledAt < rhs.event.scheduledAt
+//    }
+// }
 
-struct EventContext: Comparable, Identifiable {
+struct EventContext: Comparable, Identifiable, Hashable {
     let event: Event
     let task: Task<PICSTaskContext>
-    
     
     var id: Event.ID {
         event.id
     }
     
-    
     static func < (lhs: EventContext, rhs: EventContext) -> Bool {
         lhs.event.scheduledAt < rhs.event.scheduledAt
+    }
+
+    static func == (lhs: EventContext, rhs: EventContext) -> Bool {
+        lhs.event == rhs.event && lhs.task == rhs.task
+    }
+
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(event)
+        hasher.combine(task)
     }
 }
--- PICS/Schedule/PICSScheduler.swift
+++ PICS/Schedule/PICSScheduler.swift
@@ -16,7 +16,33 @@ typealias PICSScheduler = Scheduler<PICSTaskContext>
 
 
 extension PICSScheduler {
-    static var socialSupportTask: SpeziScheduler.Task<PICSTaskContext> {
+    static var PHQ4Task: SpeziScheduler.Task<PICSTaskContext> {
+            let dateComponents: DateComponents
+            if FeatureFlags.testSchedule {
+                // Adds a task at the current time + 1 minute for UI testing
+                dateComponents = DateComponents(
+                    hour: Calendar.current.component(.hour, from: .now),
+                    minute: Calendar.current.component(.minute, from: .now).addingReportingOverflow(1).partialValue
+                )
+            } else {
+                // Schedule the task for every day at 8:00 AM for normal app usage
+                dateComponents = DateComponents(hour: 8, minute: 0)
+            }
+
+            return Task(
+                title: String(localized: "PHQ-4_TITLE"),
+                description: String(localized: "PHQ-4_DESCRIPTION"),
+                schedule: Schedule(
+                    start: Calendar.current.startOfDay(for: Date()),
+                    repetition: .matching(dateComponents),
+                    end: .numberOfEvents(365)
+                ),
+                notifications: true,
+                context: PICSTaskContext.questionnaire(Bundle.main.questionnaire(withName: "PHQ-4"))
+            )
+        }
+    
+    static var EQ5D5LTask: SpeziScheduler.Task<PICSTaskContext> {
         let dateComponents: DateComponents
         if FeatureFlags.testSchedule {
             // Adds a task at the current time for UI testing if the `--testSchedule` feature flag is set
@@ -25,25 +51,77 @@ extension PICSScheduler {
                 minute: Calendar.current.component(.minute, from: .now)
             )
         } else {
-            // For the normal app usage, we schedule the task for every day at 8:00 AM
-            dateComponents = DateComponents(hour: 8, minute: 0)
+            // For the normal app usage, we schedule the task for every day at 9:00 AM
+            dateComponents = DateComponents(hour: 9, minute: 0)
         }
 
         return Task(
-            title: String(localized: "TASK_PROM_QUESTIONNAIRE_TITLE"),
-            description: String(localized: "TASK_PROM_QUESTIONNAIRE_DESCRIPTION"),
+            title: String(localized: "EQ5D5L_TITLE"),
+            description: String(localized: "EQ5D5L_DESCRIPTION"),
             schedule: Schedule(
                 start: Calendar.current.startOfDay(for: Date()),
                 repetition: .matching(dateComponents),
                 end: .numberOfEvents(365)
             ),
             notifications: true,
-            context: PICSTaskContext.questionnaire(Bundle.main.questionnaire(withName: "PROM-Questionnaire"))
+            context: PICSTaskContext.questionnaire(Bundle.main.questionnaire(withName: "EQ5D5L"))
         )
     }
+    
+    static var MiniNutritionalTask: SpeziScheduler.Task<PICSTaskContext> {
+        let dateComponents: DateComponents
+        if FeatureFlags.testSchedule {
+            // Adds a task at the current time for UI testing if the `--testSchedule` feature flag is set
+            dateComponents = DateComponents(
+                hour: Calendar.current.component(.hour, from: .now),
+                minute: Calendar.current.component(.minute, from: .now)
+            )
+        } else {
+            // For the normal app usage, we schedule the task for every day at 9:00 AM
+            dateComponents = DateComponents(hour: 10, minute: 0)
+        }
+
+        return Task(
+            title: String(localized: "MiniNutritional_TITLE"),
+            description: String(localized: "MiniNutritional_DESCRIPTION"),
+            schedule: Schedule(
+                start: Calendar.current.startOfDay(for: Date()),
+                repetition: .matching(dateComponents),
+                end: .numberOfEvents(365)
+            ),
+            notifications: true,
+            context: PICSTaskContext.questionnaire(Bundle.main.questionnaire(withName: "Self-MNA"))
+        )
+    }
+    
+//    static var MiniNutritionlTask: SpeziScheduler.Task<PICSTaskContext> {
+//        let dateComponents: DateComponents
+//        if FeatureFlags.testSchedule {
+//            // Adds a task at the current time for UI testing if the `--testSchedule` feature flag is set
+//            dateComponents = DateComponents(
+//                hour: Calendar.current.component(.hour, from: .now),
+//                minute: Calendar.current.component(.minute, from: .now)
+//            )
+//        } else {
+//            // For the normal app usage, we schedule the task for every day at 9:00 AM
+//            dateComponents = DateComponents(hour: 10, minute: 0)
+//        }
+//
+//        return Task(
+//            title: String(localized: "MiniNutritional_TITLE"),
+//            description: String(localized: "MiniNutritional_DESCRIPTION"),
+//            schedule: Schedule(
+//                start: Calendar.current.startOfDay(for: Date()),
+//                repetition: .matching(dateComponents),
+//                end: .numberOfEvents(365)
+//            ),
+//            notifications: true,
+//            context: PICSTaskContext.questionnaire(Bundle.main.questionnaire(withName: "PHQ-4"))
+//        )
+//    }
 
     /// Creates a default instance of the ``PICSScheduler`` by scheduling the tasks listed below.
     convenience init() {
-        self.init(tasks: [Self.socialSupportTask])
+        self.init(tasks: [Self.PHQ4Task, Self.EQ5D5LTask, Self.MiniNutritionalTask])
     }
 }
