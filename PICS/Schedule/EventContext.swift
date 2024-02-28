6759997d348f368efa2bebd470b3f4a4fc0fe007
karansoin <64022954+karansoin@users.noreply.github.com>
2024-02-27 16:49:33 -0800

    removing all commented out code

--- PICS/Schedule/EventContext.swift
+++ PICS/Schedule/EventContext.swift
@@ -22,25 +22,3 @@ import SpeziScheduler
         lhs.event.scheduledAt < rhs.event.scheduledAt
     }
  }
-
-// struct EventContext: Comparable, Identifiable, Hashable {
-//    let event: Event
-//    let task: Task<PICSTaskContext>
-//    
-//    var id: Event.ID {
-//        event.id
-//    }
-//    
-//    static func < (lhs: EventContext, rhs: EventContext) -> Bool {
-//        lhs.event.scheduledAt < rhs.event.scheduledAt
-//    }
-//
-//    static func == (lhs: EventContext, rhs: EventContext) -> Bool {
-//        lhs.event == rhs.event && lhs.task == rhs.task
-//    }
-//
-//    func hash(into hasher: inout Hasher) {
-//        hasher.combine(event)
-//        hasher.combine(task)
-//    }
-// }
