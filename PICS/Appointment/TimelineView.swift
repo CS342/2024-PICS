c37e3324f19d72697109cf31feb5be81a4754a3a
abhat01 <akanshya@slac.stanford.edu>
2024-02-27 17:53:31 -0800

    update color and fix swift line errors

--- PICS/Appointment/TimelineView.swift
+++ PICS/Appointment/TimelineView.swift
@@ -8,7 +8,7 @@
 import Foundation
 import SwiftUI
 
-struct BidirectionalArrow: Shape {
+struct CurrentLocation: Shape {
     let date1: Date
     let date2: Date
     let now: Date
@@ -24,13 +24,8 @@ struct BidirectionalArrow: Shape {
         
         let arrowWidth = rect.width
         let arrowHeight = rect.height / 2
-        
-        // Draw the main arrow line
-        path.move(to: CGPoint(x: 0, y: arrowHeight / 2))
-        path.addLine(to: CGPoint(x: arrowWidth, y: arrowHeight / 2))
-        
        
-        let circleRadius: CGFloat = 5
+        let circleRadius: CGFloat = 7
         let positions: [CGFloat] = [1, 2, 3, 4, 5].map { CGFloat($0) / 6 }
         
         for position in positions {
@@ -41,18 +36,48 @@ struct BidirectionalArrow: Shape {
                 width: circleRadius * 2,
                 height: circleRadius * 2
             )
-            if (position == 1 / 3) || (position == 2 / 3) {
-                path.addEllipse(in: circleRect)
-            }
             if (now < date1 && position == 1 / 6) ||
                 (now == date1 && position == 2 / 6) ||
                 (now > date1 && now < date2 && position == 3 / 6) ||
                 (now == date2 && position == 4 / 6) ||
                 (now > date2 && position == 5 / 6 ) {
-                //path.fill(Color.blue)
                 path.addEllipse(in: circleRect)
             }
         }
+        
+        return path
+    }
+}
+
+struct BidirectionalArrow: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        let arrowWidth = rect.width
+        let arrowHeight = rect.height / 2
+        
+        // Draw the main arrow line
+        path.move(to: CGPoint(x: 0, y: arrowHeight / 2))
+        path.addLine(to: CGPoint(x: arrowWidth, y: arrowHeight / 2))
+        
+       
+        let circleRadius: CGFloat = 5
+        
+        let circleRect1 = CGRect(
+            x: arrowWidth * 1 / 3 - circleRadius,
+            y: arrowHeight / 2 - circleRadius,
+            width: circleRadius * 2,
+            height: circleRadius * 2
+        )
+        path.addEllipse(in: circleRect1)
+        
+        let circleRect2 = CGRect(
+            x: arrowWidth * 2 / 3 - circleRadius,
+            y: arrowHeight / 2 - circleRadius,
+            width: circleRadius * 2,
+            height: circleRadius * 2
+        )
+        path.addEllipse(in: circleRect2)
         
         // Draw the right arrowhead lines
         path.move(to: CGPoint(x: arrowWidth - arrowHeight / 2, y: arrowHeight / 4))
@@ -65,7 +90,6 @@ struct BidirectionalArrow: Shape {
         path.addLine(to: CGPoint(x: 0, y: arrowHeight / 2))
         path.addLine(to: CGPoint(x: arrowHeight / 2, y: arrowHeight * 3 / 4))
         path.closeSubpath()
-                
         
         return path
     }
@@ -86,10 +110,14 @@ struct TimelineView: View {
 
         if let date1 = calendar.date(from: components1), let date2 = calendar.date(from: components2), let now = calendar.date(from: components3) {
             VStack {
-                BidirectionalArrow(date1: date1, date2: date2, now: now)
+                BidirectionalArrow()
                     .fill(Color.black)
                     .stroke(Color.black, lineWidth: 2)
                     .frame(width: 361, height: 25)
+                    .overlay(
+                        CurrentLocation(date1: date1, date2: date2, now: now)
+                            .fill(Color.accentColor)
+                    )
                 HStack(spacing: 0) {
                     Spacer()
                     Text("Appointment 1")
