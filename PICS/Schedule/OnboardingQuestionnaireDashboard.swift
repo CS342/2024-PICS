5a13c511467b2ff86147847d75e19a8fd0825b31
Catherine Zhang <137839789+czhang771@users.noreply.github.com>
2024-02-28 20:09:32 -0800

    Fix licensing

--- PICS/Schedule/OnboardingQuestionnaireDashboard.swift
+++ PICS/Schedule/OnboardingQuestionnaireDashboard.swift
@@ -1,3 +1,10 @@
+//
+// This source file is part of the PICS based on the Stanford Spezi Template Application project
+//
+// SPDX-FileCopyrightText: 2023 Stanford University
+//
+// SPDX-License-Identifier: MIT
+//
 import SpeziOnboarding
 import SpeziQuestionnaire
 import SwiftUI
