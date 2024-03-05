59a5746ac6f085df25b025ad455b3d28626b8f6e
abhat01 <akanshya@slac.stanford.edu>
2024-03-04 17:20:09 -0800

    Update NotificationPermissions.swift

--- PICS/Onboarding/NotificationPermissions.swift
+++ PICS/Onboarding/NotificationPermissions.swift
@@ -1,7 +1,7 @@
 //
 // This source file is part of the Example Application based on the Stanford Spezi Template Application project
 //
-// SPDX-FileCopyrightText: 2023 Stanford University
+// SPDX-FileCopyrightText: 2024 Stanford University
 //
 // SPDX-License-Identifier: MIT
 //
