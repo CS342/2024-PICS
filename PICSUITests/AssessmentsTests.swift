0e30e2eeee876d27bb85259b2d9e24535cbedcde
karansoin <64022954+karansoin@users.noreply.github.com>
2024-03-06 02:06:55 -0800

    undoing added tests to save for future testing and code coverage

--- PICSUITests/AssessmentsTests.swift
+++ PICSUITests/AssessmentsTests.swift
@@ -8,26 +8,13 @@
 
 import XCTest
 
-final class AssessmentsUITests: XCTestCase {
+class AssessmentsTests: XCTestCase {
     override func setUpWithError() throws {
-        try super.setUpWithError()
         continueAfterFailure = false
-        
-        let app = XCUIApplication()
-        app.launchArguments = ["--skipOnboarding"]
-        app.launch()
+        XCUIApplication().launch()
     }
-    func testAssessmentsViewLoads() throws {
+
+    func testAssessmentViewPresence() {
         let app = XCUIApplication()
-        
-        // Navigate to the Assessments view
-        app.buttons["Assessments"].tap()
-        
-        // Assert that the Assessments view is displayed
-        XCTAssertTrue(app.staticTexts["ASSESSMENTS_NAVIGATION_TITLE"].waitForExistence(timeout: 2))
-        
-        // Assert that the Trail Making and Stroop Test sections are displayed
-        XCTAssertTrue(app.staticTexts["Trail Making"].waitForExistence(timeout: 2))
-        XCTAssertTrue(app.staticTexts["Stroop Test"].waitForExistence(timeout: 2))
     }
 }
