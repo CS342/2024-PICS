217e0dbcdcd602b003794f98177c6df4b1efce2a
karansoin <64022954+karansoin@users.noreply.github.com>
2024-03-06 01:55:39 -0800

    attempt at adding more static assessment tests

--- PICSUITests/AssessmentsTests.swift
+++ PICSUITests/AssessmentsTests.swift
@@ -8,13 +8,47 @@
 
 import XCTest
 
-class AssessmentsTests: XCTestCase {
-    override func setUpWithError() throws {
-        continueAfterFailure = false
-        XCUIApplication().launch()
-    }
+//class AssessmentsTests: XCTestCase {
+//    override func setUpWithError() throws {
+//        continueAfterFailure = false
+//        XCUIApplication().launch()
+//    }
+//
+//    func testAssessmentsViewLoads() throws {
+//        let app = XCUIApplication()
+//        
+//        // Navigate to the Assessments view
+//        app.buttons["Assessments"].tap()
+//        
+//        // Assert that the Assessments view is displayed
+//        XCTAssertTrue(app.staticTexts["ASSESSMENTS_NAVIGATION_TITLE"].waitForExistence(timeout: 2))
+//        
+//        // Assert that the Trail Making and Stroop Test sections are displayed
+//        XCTAssertTrue(app.staticTexts["Trail Making"].waitForExistence(timeout: 2))
+//        XCTAssertTrue(app.staticTexts["Stroop Test"].waitForExistence(timeout: 2))
+//    }
+//}
+final class AssessmentsUITests: XCTestCase {
 
-    func testAssessmentViewPresence() {
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        continueAfterFailure = false
+        
         let app = XCUIApplication()
+        app.launchArguments = ["--skipOnboarding"]
+        app.launch()
+    }
+    func testAssessmentsViewLoads() throws {
+        let app = XCUIApplication()
+        
+        // Navigate to the Assessments view
+        app.buttons["Assessments"].tap()
+        
+        // Assert that the Assessments view is displayed
+        XCTAssertTrue(app.staticTexts["ASSESSMENTS_NAVIGATION_TITLE"].waitForExistence(timeout: 2))
+        
+        // Assert that the Trail Making and Stroop Test sections are displayed
+        XCTAssertTrue(app.staticTexts["Trail Making"].waitForExistence(timeout: 2))
+        XCTAssertTrue(app.staticTexts["Stroop Test"].waitForExistence(timeout: 2))
     }
 }
