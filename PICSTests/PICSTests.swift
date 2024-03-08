58ac82c811ed44d758a85da740db752b3c9bdd93
karansoin <64022954+karansoin@users.noreply.github.com>
2024-03-08 13:09:31 -0800

    adding functional tests and updating unit testing settings

--- PICSTests/PICSTests.swift
+++ PICSTests/PICSTests.swift
@@ -8,10 +8,25 @@
 
 @testable import PICS
 import XCTest
+import XCTSpezi
 
 
 class PICSTests: XCTestCase {
     func testExample() throws {
         XCTAssertTrue(true)
+    }
+    // Testing the PICSScheduler initialization
+    func testInitializationWithDefaultTasks() async throws {
+        let scheduler = PICSScheduler()
+        withDependencyResolution {
+            scheduler
+        }
+        try await Task.sleep(for: .milliseconds(500))
+        XCTAssertNotEqual(scheduler.tasks.count, 0)
+        print("Scheduler has \(scheduler.tasks.count) tasks:")
+        scheduler.tasks.forEach { task in
+            print("Task title: \(task.title)")
+        }
+        XCTAssertTrue(scheduler.tasks.contains(where: { $0.title == "PHQ-4: Patient Psychological Health Questionnaire" }))
     }
 }
