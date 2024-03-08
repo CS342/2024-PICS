16c3b22ed3b1307f5423a3e7e735e292a274df0b
abhat01 <akanshyabhat@gmail.com>
2024-03-07 17:55:58 -0800

    contacts testing

---
+++ PICSUITests/ContactsTests.swift
@@ -0,0 +1,28 @@
+//
+// This source file is part of the PICS based on the Stanford Spezi Template Application project
+//
+// SPDX-FileCopyrightText: 2024 Stanford University
+//
+// SPDX-License-Identifier: MIT
+//
+
+import XCTest
+
+
+class ContactsTests: XCTestCase {
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        
+        continueAfterFailure = false
+        
+        let app = XCUIApplication()
+        app.launchArguments = ["--skipOnboarding"]
+        app.launch()
+    }
+    
+    func testApplicationLaunch() throws {
+        let app = XCUIApplication()
+        XCTAssertTrue(app.buttons["Contacts"].waitForExistence(timeout: 2))
+        app.buttons["Health"].tap()
+    }
+}
