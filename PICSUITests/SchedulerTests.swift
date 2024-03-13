b7cfd05d51e0ccda73e38d9c8afb79bb19e94341
karansoin <64022954+karansoin@users.noreply.github.com>
2024-03-12 17:04:46 -0700

    adding the scheduler+questionnaire UI tests files

---
+++ PICSUITests/SchedulerTests.swift
@@ -0,0 +1,11 @@
+//
+// This source file is part of the PICS to test the assessments task tab.
+//
+// SPDX-FileCopyrightText: 2024 Stanford University
+//
+// SPDX-License-Identifier: MIT
+//
+
+import XCTest
+
+
